cba11e080d60ffa10294f97caa5b55b5001db052
ikngtty <ikngtty@gmail.com>
2021-05-16 03:01:00 +0900

    :green_heart: organize values into YouTubeComment

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 function main() {
   const lastCheckedAt = new Date(ps.getProperty('LAST_CHECKED_AT'));
   const checkedAt = new Date();
-  const comments = fetchComments(lastCheckedAt, checkedAt);
-  console.info('fetched comments', comments);
+  const comments = fetchYouTubeComments(lastCheckedAt, checkedAt);
+  comments.forEach(comment => {
+    console.info('fetched comment', comment);
+  });
   ps.setProperty('LAST_CHECKED_AT', checkedAt.toISOString());
 }
 
-function fetchComments(from, to) {
+function fetchYouTubeComments(from, to) {
   const isTargetComment = comment => {
     const updatedAt = Date.parse(comment.snippet.updatedAt);
     return from < updatedAt && updatedAt <= to;
@@ -20,7 +22,7 @@ function fetchComments(from, to) {
   });
   console.log('playlistItemsResult', playlistItemsResult);
 
-  const fetched = [];
+  const targets = [];
   playlistItemsResult.items.forEach(playlistItem => {
     console.log('playlistItem', playlistItem);
     // TODO: fetch all pages
@@ -33,7 +35,7 @@ function fetchComments(from, to) {
       const topLevelComment = commentThread.snippet.topLevelComment;
       console.log('topLevelComment', topLevelComment);
       if (isTargetComment(topLevelComment)) {
-        fetched.push(topLevelComment);
+        targets.push(YouTubeComment.fromTopLevelComment(topLevelComment));
       }
 
       if (!commentThread.replies) {
@@ -42,13 +44,44 @@ function fetchComments(from, to) {
       commentThread.replies.comments.forEach(reply => {
         console.log('reply', reply);
         if (isTargetComment(reply)) {
-          // HACK: circular dependency
-          // Had better to move values to a new object.
-          reply.snippet.parentComment = topLevelComment;
-          fetched.push(reply);
+          targets.push(YouTubeComment.fromReply(reply, topLevelComment));
         }
       });
     });
   });
-  return fetched;
+  return targets;
+}
+
+class YouTubeComment {
+  static fromTopLevelComment(topLevelComment) {
+    const comment = new YouTubeComment();
+    comment.textOriginal = topLevelComment.snippet.textOriginal;
+    comment.publishedAt = topLevelComment.snippet.publishedAt;
+    comment.updatedAt = topLevelComment.snippet.updatedAt;
+    comment.authorDisplayName = topLevelComment.snippet.authorDisplayName;
+    comment.authorProfileImageUrl = topLevelComment.snippet.authorProfileImageUrl;
+    comment.videoId = topLevelComment.snippet.videoId;
+    comment.parentComment = null;
+    return comment;
+  }
+
+  static fromReply(reply, topLevelComment) {
+    const comment = new YouTubeComment();
+    comment.textOriginal = reply.snippet.textOriginal;
+    comment.publishedAt = reply.snippet.publishedAt;
+    comment.updatedAt = reply.snippet.updatedAt;
+    comment.authorDisplayName = reply.snippet.authorDisplayName;
+    comment.authorProfileImageUrl = reply.snippet.authorProfileImageUrl;
+    comment.videoId = reply.snippet.videoId;
+    comment.parentComment = this.fromTopLevelComment(topLevelComment);
+    return comment;
+  }
+
+  get isReply() {
+    return !!this.parentComment
+  }
+
+  get videoUrl() {
+    return `https://www.youtube.com/watch?v=${this.videoId}`;
+  }
 }
