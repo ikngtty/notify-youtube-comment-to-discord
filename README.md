5b3af515327b14d51e94bb819649f5ec8053f3a9
ikngtty <ikngtty@gmail.com>
2021-05-15 17:54:41 +0900

    :sparkles: fetch YouTube comments of a playlist

---
+++ README.md
@@ -0,0 +1,9 @@
+# Notify YouTube comment to Discord
+
+A Google Apps Script project to notify YouTube comment to Discord.
+
+## Note
+
+Using `clasp` with `--rootDir ./src` option.
+
+This script uses YouTube API, so it spends API quotas.
---
+++ src/Code.js
@@ -0,0 +1,27 @@
+function checkComments() {
+  // TODO: fetch all pages
+  const playlistItemsResult = YouTube.PlaylistItems.list('snippet,contentDetails', {
+    // fields: 'items(contentDetails(videoId),snippet(title))',
+    maxResults: 50,
+    playlistId: 'ENTER YOUR PLAYLIST ID',
+  });
+  console.log('playlistItemsResult', playlistItemsResult);
+  playlistItemsResult.items.forEach(playlistItem => {
+    console.log('playlistItem', playlistItem);
+    // TODO: fetch all pages
+    const commentThreadsResult = YouTube.CommentThreads.list('snippet,replies', {
+      maxResults: 100,
+      videoId: playlistItem.contentDetails.videoId,
+    });
+    console.log('commentThreadsResult', commentThreadsResult);
+    commentThreadsResult.items.forEach(commentThread => {
+      console.info('topLevelComment', commentThread.snippet.topLevelComment);
+      if (!commentThread.replies) {
+        return;
+      }
+      commentThread.replies.comments.forEach(reply => {
+        console.info('reply', reply);
+      });
+    });
+  });
+}
