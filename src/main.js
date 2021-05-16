04ab0362486b39cbda7c9ee59463d537b721f9a8
ikngtty <ikngtty@gmail.com>
2021-05-17 02:42:47 +0900

    :sparkles: add video info to fetch results

--- src/main.js
+++ src/main.js
@@ -22,16 +22,18 @@ function fetchYouTubeComments(from, to) {
   const targets = [];
   playlistItemsResult.items.forEach(playlistItem => {
     console.log('playlistItem', playlistItem);
+    const video = YouTubeVideo.fromPlaylistItem(playlistItem);
+
     // TODO: fetch all pages
     const commentThreadsResult = YouTube.CommentThreads.list('snippet,replies', {
       maxResults: 100,
-      videoId: playlistItem.contentDetails.videoId,
+      videoId: video.id,
     });
     console.log('commentThreadsResult', commentThreadsResult);
     commentThreadsResult.items.forEach(commentThread => {
       const topLevelComment_ = commentThread.snippet.topLevelComment;
       console.log('topLevelComment_', topLevelComment_);
-      const topLevelComment = new YouTubeComment(topLevelComment_, null);
+      const topLevelComment = new YouTubeComment(video, topLevelComment_, null);
 
       if (isTargetComment(topLevelComment)) {
         targets.push(topLevelComment);
@@ -42,7 +44,7 @@ function fetchYouTubeComments(from, to) {
       }
       commentThread.replies.comments.forEach(reply_ => {
         console.log('reply_', reply_);
-        const reply = new YouTubeComment(reply_, topLevelComment);
+        const reply = new YouTubeComment(video, reply_, topLevelComment);
 
         if (isTargetComment(reply)) {
           targets.push(reply);
@@ -53,22 +55,31 @@ function fetchYouTubeComments(from, to) {
   return targets;
 }
 
+class YouTubeVideo {
+  static fromPlaylistItem(playlistItem) {
+    const video = new YouTubeVideo();
+    video.id = playlistItem.contentDetails.videoId;
+    video.title = playlistItem.snippet.title;
+    return video;
+  }
+
+  get url() {
+    return `https://www.youtube.com/watch?v=${this.videoId}`;
+  }
+}
+
 class YouTubeComment {
-  constructor(comment, parentYouTubeComment) {
+  constructor(youTubeVideo, comment, parentYouTubeComment) {
+    this.video = youTubeVideo;
     this.textOriginal = comment.snippet.textOriginal;
     this.publishedAt = Date.parse(comment.snippet.publishedAt);
     this.updatedAt = Date.parse(comment.snippet.updatedAt);
     this.authorDisplayName = comment.snippet.authorDisplayName;
     this.authorProfileImageUrl = comment.snippet.authorProfileImageUrl;
-    this.videoId = comment.snippet.videoId;
     this.parentComment = parentYouTubeComment;
   }
 
   get isReply() {
     return !!this.parentComment
-  }
-
-  get videoUrl() {
-    return `https://www.youtube.com/watch?v=${this.videoId}`;
   }
 }
